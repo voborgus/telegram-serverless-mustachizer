4d9cc218a81c4c77596a1883246563ba02af2952
Dmitrii Sugrobov <dmitrii.sugrobov@leroymerlin.ru>
2021-12-15 00:27:15 +0300

    first

--- terraform/telegram-handler.tf
+++ terraform/telegram-handler.tf
@@ -6,16 +6,7 @@ resource "yandex_function" "telegram_handler" {
   description = "Handle incoming messages from telegram"
   execution_timeout = "15"
   environment = {
-    "AWS_ACCESS_KEY_ID": var.s3_config.access_key,
-    "AWS_SECRET_ACCESS_KEY": var.s3_config.secret_key,
-    "BUCKET_ID": var.s3_config.bucket,
-    "bot_token": var.bot_token,
-    "dynamodb_table": var.dynamodb_table,
-    "region": var.s3_config.region,
-    "db_id": yandex_ydb_database_serverless.database1.id,
-    "cloud_id": var.yc_cloud_id,
-    "queue_id": var.queue_id,
-    "queue_name": var.queue_name
+    "bot_token": var.bot_token
   }
   user_hash = filesha256(data.archive_file.code_archive.output_path)
   content {
--- terraform/ydb.tf
+++
@@ -1,8 +0,0 @@
-resource "yandex_ydb_database_serverless" "database1" {
-  name      = var.dynamodb_table
-  folder_id = var.yc_folder_id
-}
-
-output "yandex_ydb_database_serverless_id" {
-  value = yandex_ydb_database_serverless.database1.id
-}
